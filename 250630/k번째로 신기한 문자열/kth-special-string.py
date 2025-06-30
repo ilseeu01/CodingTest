0afe63ec2f86fe1806a00dcfd0a74ddf9a70556a
ilseeu01 <161840918+ilseeu01@users.noreply.github.com>
2025-06-30 15:50:54 +0900

    Title: k번째로 신기한 문자열, Time: 81ms, Memory: 19MB, Status: Passed - Codetree

--- 250630/k번째로 신기한 문자열/kth-special-string.py
+++ 250630/k번째로 신기한 문자열/kth-special-string.py
@@ -1,13 +1,23 @@
+import string
 n, k, t = input().split()
 n = int(n)
 k = int(k)
 
 arr = [input().strip() for _ in range(n)]
 
-sorted_arr = []
-for i in range(n):
-    if t in arr[i]:
-        sorted_arr.append(arr[i])
+# 문자열 클린 함수: 제어 문자 제거
+def is_clean(s):
+    return all(ch in string.printable and ch not in string.whitespace[1:] for ch in s)
 
-arr.sort()
-print(arr[k])
+# 클린 + 필터링
+filtered_arr = []
+
+for word in arr:
+    if not is_clean(word):
+        continue  # 이상한 문자 하나라도 있으면 스킵
+    if t in word:
+        filtered_arr.append(word)
+
+filtered_arr.sort()
+print(filtered_arr[k-1])
+
