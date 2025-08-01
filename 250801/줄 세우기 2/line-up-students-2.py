de3be8e687ea968e95df8f3c30550a4ad1729e96
ilseeu01 <161840918+ilseeu01@users.noreply.github.com>
2025-08-01 12:51:25 +0900

    Title: 줄 세우기 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250801/줄 세우기 2/line-up-students-2.py
+++ 250801/줄 세우기 2/line-up-students-2.py
@@ -5,12 +5,12 @@ class Student:
         self.idx = idx
 
 n = int(input())
-student = [Student(*input().split()) for _ in range(n)]
+student = [Student(*input().split(), i) for i in range(n)]
 for stu in student:
     stu.height = int(stu.height)
     stu.weight = int(stu.weight)
 
 student.sort(key=lambda x: (x.height, -x.weight))
 
-for idx, value in enumerate(student):
-    print(f"{value.height} {value.weight} {idx+1}")
+for value in student:
+    print(f"{value.height} {value.weight} {value.idx+1}")
