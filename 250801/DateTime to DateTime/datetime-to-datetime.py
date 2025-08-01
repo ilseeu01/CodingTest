2d4d382a8ec66c29bacdbc7a948b9cafde51e842
ilseeu01 <161840918+ilseeu01@users.noreply.github.com>
2025-08-01 19:06:29 +0900

    Title: DateTime to DateTime, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250801/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,11 @@
+import sys
+
+a, b, c = map(int, input().split())
+
+if a <= 11 and b <= 11 and c <= 11:
+    print(-1)
+    sys.exit(0)
+
+elasped_min = (a - 11) * 24 * 60 + (b - 11) * 60 + (c - 11)
+print(elasped_min)
+
