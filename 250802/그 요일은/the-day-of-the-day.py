fd2e8f0f5b18c71ec544047ef8c112f11f893b7f
ilseeu01 <161840918+ilseeu01@users.noreply.github.com>
2025-08-02 16:08:07 +0900

    Title: 그 요일은, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250802/그 요일은/the-day-of-the-day.py
+++ 250802/그 요일은/the-day-of-the-day.py
@@ -2,10 +2,7 @@ m1, d1, m2, d2 = map(int, input().split())
 day = input()
 #                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-#days = [" ", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-
-
 
 elapsed_days1 = 0
 for i in range(m1):
@@ -20,13 +17,14 @@ for i in range(m2):
 
 elapsed_days2 += d2
 day2 = elapsed_days2 % 7
-
-res = (elapsed_days2 - elapsed_days1) % 7
+#print(elapsed_days2, elapsed_days1)
+rest = (elapsed_days2 - elapsed_days1) % 7
+res = (elapsed_days2 - elapsed_days1) // 7
 
 for i in range(6):
     if day == days[i]:
         idx = i
 
-if res < idx:
-    res -= 1
+if idx <= rest:
+    res += 1
 print(res)
