4e3ff003c65ebd9eca7148fc42957935ed9f2ca9
ilseeu01 <161840918+ilseeu01@users.noreply.github.com>
2025-08-02 12:40:10 +0900

    Title: 그 요일은, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250802/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,28 @@
+m1, d1, m2, d2 = map(int, input().split())
+day = input()
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#days = [" ", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+for i in range(6):
+    if day == days[i]:
+        idx = i
+
+elapsed_days1 = 0
+for i in range(m1):
+    elapsed_days1 += num_of_days[i]
+
+elapsed_days1 += d1
+day1 = elapsed_days1 % 7 #이게 월요일
+
+elapsed_days2 = 0
+for i in range(m2):
+    elapsed_days2 += num_of_days[i]
+
+elapsed_days2 += d2
+day2 = elapsed_days2 % 7
+
+#print(days[(day2 - day1) % 7])
+res = (elapsed_days2 - elapsed_days1) % 7
+print(res)
